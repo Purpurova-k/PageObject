e5198bf3c824593ba7f8cc71780353804131f812
Purpurova-k <katrin.purpurova@gmail.com>
2022-02-11 17:31:39 +0300

    DataHelper & PageObjects added

---
+++ src/test/java/ru/netology/web/page/DashboardPage.java
@@ -0,0 +1,40 @@
+package ru.netology.web.page;
+
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+import lombok.val;
+import ru.netology.web.data.DataHelper;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+public class DashboardPage {
+    private SelenideElement heading = $("h1");
+    private ElementsCollection cards = $$(".list__item");
+    private final String balanceStart = "баланс: ";
+    private final String balanceFinish = " р.";
+    private SelenideElement reloadButton = $("[data-test-id=action-reload]");
+
+
+    public DashboardPage() {
+        heading.shouldBe(visible).shouldHave(text("Ваши карты"));
+    }
+
+    public int getCardBalance(String lastNumberCard) {
+        val balance = cards.findBy(text(lastNumberCard)).text();
+        val start = balance.indexOf(balanceStart);
+        val finish = balance.indexOf(balanceFinish);
+        val value = balance.substring(start + balanceStart.length(), finish).trim();
+        return Integer.parseInt(value);
+    }
+
+    public TransferPage transferMoney(String lastNumbersCard) {
+        var replenishButton = cards.findBy(text(lastNumbersCard)).$("[data-test-id=action-deposit]");
+        replenishButton.click();
+        return new TransferPage();
+    }
+
+
+}
